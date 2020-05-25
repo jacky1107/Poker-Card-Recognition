e5287fb58ecfd9f23c2f540a63a2abec7e8e392d
Jacky <jacky110711071107@gmail.com>
2020-05-25 21:19:11 +0800

    delete .DS_Store

--- backup/v1/data.py
+++
@@ -1,8 +0,0 @@
-def getData():
-    return {
-        "data": "data",
-        "edges": "edges",
-        "test": "testData",
-        "poker": "allPokers",
-        "train": "trainData",
-    }
--- removeDs.py
+++
@@ -1,8 +0,0 @@
-import os
-
-c = os.getcwd()
-print(os.listdir(c))
-for data in os.listdir(c):
-    apth = os.path.join(c, data)
-    if "DS_Store" in data:
-        os.remove(apth)
